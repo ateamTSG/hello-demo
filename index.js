787bbaa8ef4f0fde3f90a7ead1768acb3384dd95
A Team <68806181+ateamTSG@users.noreply.github.com>
2020-08-02 16:53:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app     = express();
  
 app.get('/', function(req,res){
-  res.send('צחי הגבר גברי');
+  res.send('This is a demonstration of full CI/CD using Azure Devops & ARO - © A-Team TSG');
 });
  
 var port = 3000;
